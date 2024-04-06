0ea82de843201bf9ed16f4edc5f456abd2b84e73
Jonathan Cruz <cajonathan1@gmail.com>
2024-04-05 22:05:34 -0400

    Adds image rendering and styling

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,35 +1,40 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import products from '../../../assets/data/products'
+import Colors from '../constants/Colors';
 
-const product = products
+const product = products[1]
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Pizza Peperoni</Text>
-      <Text style={styles.price}>$12.99</Text>
+      <Image source={{ uri: product.image }} style={styles.image} />
+
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>${product.price}</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 20,
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+
+  image: {
+    width: '100%',
+    aspectRatio: 1,
   },
+
   title: {
     fontSize: 20,
     fontWeight: '600', 
     marginVertical: 10,
   },
   price: {
-    color: 'blue',
-    
+    color: Colors.light.tint,
+    fontWeight: 'bold',
+
   },
 });
