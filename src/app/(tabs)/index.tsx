2d0b264c145ec468128c387b66bf4fcf24306295
Jonathan Cruz <cajonathan1@gmail.com>
2024-04-06 22:33:07 -0400

    - Cleans and Tidies up code - Adds custom component that links products

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,40 +1,13 @@
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { View } from 'react-native';
 import products from '../../../assets/data/products'
-import Colors from '../constants/Colors';
+import ProductListItem from '../components/ProductListItem';
 
-const product = products[1]
+export default function MenuScreen() {
+  return ( 
+  <View>
+    <ProductListItem product={products[5]} />
+    <ProductListItem product={products[1]} />
 
-export default function TabOneScreen() {
-  return (
-    <View style={styles.container}>
-      <Image source={{ uri: product.image }} style={styles.image} />
-
-      <Text style={styles.title}>{product.name}</Text>
-      <Text style={styles.price}>${product.price}</Text>
-    </View>
+  </View>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'white',
-    padding: 10,
-    borderRadius: 20,
-  },
-
-  image: {
-    width: '100%',
-    aspectRatio: 1,
-  },
-
-  title: {
-    fontSize: 20,
-    fontWeight: '600', 
-    marginVertical: 10,
-  },
-  price: {
-    color: Colors.light.tint,
-    fontWeight: 'bold',
-
-  },
-});
+};
