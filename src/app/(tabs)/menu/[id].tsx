a9381ca45e1c1a937014401c83a807f1fac7526c
Jonathan Cruz <cajonathan1@gmail.com>
2024-04-11 20:55:03 -0400

    - Reorganizes code structure - Fixes bugs

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,11 +1,14 @@
 import { useLocalSearchParams } from 'expo-router';
 import { View, Text } from 'react-native'
+import { Stack } from "expo-router";
+
 
 const ProductDetailsScreen = () => {
   const { id } = useLocalSearchParams();
 
   return (
     <View>
+      <Stack.Screen options={{ title: 'Details ' + id, headerTitleAlign: 'center'}}/>
       <Text style={{fontSize: 20}}>ProductDetailsScreen for id: {id}</Text>
     </View>
   );
