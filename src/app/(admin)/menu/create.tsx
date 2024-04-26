74c8c9f1c77697ed7977966fe67eee4b4b13da68
Jonathan Cruz <cajonathan1@gmail.com>
2024-04-26 17:46:15 -0400

    - Adds styling to CreateProductScreen

--- src/app/(admin)/menu/create.tsx
+++ src/app/(admin)/menu/create.tsx
@@ -5,6 +5,9 @@ const CreateProductScreen = () => {
     <View style={styles.container}>
       <Text style={styles.label}>Name</Text>
       <TextInput placeholder='Name' style={styles.input} />
+
+      <Text style={styles.label}>Price ($)</Text>
+      <TextInput placeholder='9.99' style={styles.input} />
     </View>
   )
 }
@@ -16,10 +19,15 @@ const styles = StyleSheet.create({
         padding: 10,
     },
     input: {
-
+        backgroundColor: 'white',
+        padding: 10,
+        borderRadius: 5,
+        marginTop: 5,
+        marginBottom: 20,
     },
     label: {
-
+        color: 'gray',
+        fontSize: 16,
     },
 });
 
