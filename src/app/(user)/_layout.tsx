5ac41908902c8abfc76e41c6e8c05b63c9754901
Jonathan Cruz <cajonathan1@gmail.com>
2024-05-15 19:57:55 -0400

    - Fixes bugs

--- src/app/(user)/_layout.tsx
+++ src/app/(user)/_layout.tsx
@@ -39,7 +39,7 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="two"
+        name="orders"
         options={{
           title: 'Orders',
           headerTitleAlign: 'center',
