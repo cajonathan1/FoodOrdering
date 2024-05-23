becb49aac2c54fbb3fea4c7c8961a211b509bc1b
Jonathan Cruz <cajonathan1@gmail.com>
2024-05-23 19:14:34 -0400

    Fetches the session

--- src/app/providers/AuthProvider.tsx
+++ src/app/providers/AuthProvider.tsx
@@ -1,9 +1,22 @@
-import { PropsWithChildren, createContext } from "react";
+import { supabase } from "@/lib/supabase";
+import { Session } from "@supabase/supabase-js";
+import { PropsWithChildren, createContext, useEffect, useState } from "react";
 
 type AuthData = {};
 
 const AuthContext = createContext<AuthData>({});
 
 export default function AuthProvider({children}: PropsWithChildren) {
+    const [session, setSession] = useState<Session | null>(null);
+
+    useEffect(() => {
+        const fetchSession = async() => {
+            const { data, error } = await supabase.auth.getSession();
+            setSession(data.session);
+        };
+
+        fetchSession();
+    }, []);
+
     return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>;
 }
