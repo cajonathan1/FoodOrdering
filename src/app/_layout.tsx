50550bc2ccab05c93438727cce179d68257c7d81
Jonathan Cruz <cajonathan1@gmail.com>
2024-06-06 19:39:43 -0400

    Installs QueryProvider

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -7,6 +7,7 @@ import { useEffect } from 'react';
 import { useColorScheme } from '../app/components/useColorScheme';
 import CartProvider from './providers/CartProvider';
 import AuthProvider from './providers/AuthProvider';
+import QueryProvider from './providers/QueryProvider';
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -51,14 +52,16 @@ function RootLayoutNav() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <AuthProvider>
-        <CartProvider>
-          <Stack>
-            <Stack.Screen name="(admin)" options={{ headerShown: false }} />
-            <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-            <Stack.Screen name="(user)" options={{ headerShown: false }} />
-            <Stack.Screen name="cart" options={{ presentation: 'modal' }} />
-          </Stack>
-        </CartProvider>
+        <QueryProvider>
+          <CartProvider>
+            <Stack>
+              <Stack.Screen name="(admin)" options={{ headerShown: false }} />
+              <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+              <Stack.Screen name="(user)" options={{ headerShown: false }} />
+              <Stack.Screen name="cart" options={{ presentation: 'modal' }} />
+            </Stack>
+          </CartProvider>
+        </QueryProvider>
       </AuthProvider>
     </ThemeProvider>
   );
