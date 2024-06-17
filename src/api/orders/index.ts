63bbc644d0738fce4bc943550c32bb063e6bc97e
Jonathan Cruz <cajonathan1@gmail.com>
2024-06-17 19:14:30 -0400

    Plugged in the hook to user side

--- src/api/orders/index.ts
+++ src/api/orders/index.ts
@@ -1,11 +1,32 @@
 import { useQuery } from "@tanstack/react-query";
 import { supabase } from "@/lib/supabase";
+import { useAuth } from "@/app/providers/AuthProvider";
 
 export const useAdminOrderList = () => {
     return useQuery({
         queryKey: ['orders'],
         queryFn: async () => {
           const { data, error } = await supabase.from('orders').select('*');
+          if (error) {
+            throw new Error(error.message);
+          }
+          return data;
+        },
+      });
+};
+
+export const useMyOrderList = () => {
+    const { session } = useAuth();
+    const id = session?.user.id;
+
+    return useQuery({
+        queryKey: ['orders', { userId: id }],
+        queryFn: async () => {
+            if (!id) return null;
+          const { data, error } = await supabase
+          .from('orders')
+          .select('*')
+          .eq('user_id', id);
           if (error) {
             throw new Error(error.message);
           }
