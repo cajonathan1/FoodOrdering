ae6fc2e10e02a333eef6c5872c5021f722b53d02
Jonathan Cruz <cajonathan1@gmail.com>
2024-06-20 21:40:23 -0400

    Adds a way to subscribe to new orders as they are created

--- src/app/(admin)/orders/list/index.tsx
+++ src/app/(admin)/orders/list/index.tsx
@@ -1,9 +1,15 @@
 import { Text, FlatList, ActivityIndicator } from "react-native"; 
 import OrderListItem from "@/app/components/OrderListItem";
 import { useAdminOrderList } from "@/api/orders";
+import { useEffect } from "react";
+import { supabase } from "@/lib/supabase";
+import { useQueries, useQueryClient } from "@tanstack/react-query";
+import { useInsertOrderSubscription } from "@/api/orders/subscription";
 
 export default function OrdersScreen() {
     const { data: orders, isLoading, error } = useAdminOrderList({archived: false});
+
+    use useInsertOrderSubscription();
 
     if (isLoading) {
         return <ActivityIndicator />;
