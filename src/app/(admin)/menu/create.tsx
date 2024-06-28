235b35b7f187ef71cf1e610998ca50b3acccd0db
Jonathan Cruz <cajonathan1@gmail.com>
2024-06-27 22:14:10 -0400

    Starts work on downloading images

--- src/app/(admin)/menu/create.tsx
+++ src/app/(admin)/menu/create.tsx
@@ -6,6 +6,10 @@ import { View, Text, StyleSheet, TextInput, Image, Alert } from 'react-native'
 import * as ImagePicker from 'expo-image-picker';
 import { Stack, useLocalSearchParams, useRouter } from 'expo-router';
 import { useInsertProduct, useProduct, useUpdateProduct, useDeleteProduct } from '@/api/products';
+import * as FileSystem from 'expo-file-system';
+import { randomUUID } from 'expo-crypto';
+import { supabase } from '@/lib/supabase';
+import { decode } from 'base64-arraybuffer';
 
 const CreateProductScreen = () => {
     const [name, setName] = useState('');
@@ -66,12 +70,14 @@ const CreateProductScreen = () => {
         }
     };
 
-    const onCreate = () => {
+    const onCreate = async () => {
         if (!validateInput()) {
             return;
         }
 
-        insertProduct({ name, price: parseFloat(price), image }, 
+        const imagePath = await uploadImage();
+
+        insertProduct({ name, price: parseFloat(price), image: imagePath }, 
             {
                 onSuccess: () => {
                     resetFields();
@@ -113,6 +119,8 @@ const CreateProductScreen = () => {
         }
       };
 
+      
+
       const onDelete = () => {
         deleteProduct(id, {onSuccess:() => {
             resetFields();
@@ -131,6 +139,25 @@ const CreateProductScreen = () => {
                 onPress: onDelete,
             },
         ]);
+      };
+
+      const uploadImage = async () => {
+        if (!image?.startsWith('file://')) {
+          return;
+        }
+      
+        const base64 = await FileSystem.readAsStringAsync(image, {
+          encoding: 'base64',
+        });
+        const filePath = `${randomUUID()}.png`;
+        const contentType = 'image/png';
+        const { data, error } = await supabase.storage
+          .from('product-images')
+          .upload(filePath, decode(base64), { contentType });
+      
+        if (data) {
+          return data.path;
+        }
       };
 
   return (
--- src/app/components/ProductListItem.tsx
+++ src/app/components/ProductListItem.tsx
@@ -2,12 +2,13 @@ import { StyleSheet, Text, View, Image, Pressable } from 'react-native';
 import Colors from '../constants/Colors';
 import { Tables } from '../types';
 import { Link, useSegments } from 'expo-router';
+import RemoteImage from './RemoteImage';
 
 export const defaultPizzaImage =
-    'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png'
+  'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png'
 
 type ProductListItemProps = {
-    product: Tables<'products'>;
+  product: Tables<'products'>;
 }
 
 const ProductListItem = ({ product }: ProductListItemProps) => {
@@ -16,20 +17,19 @@ const ProductListItem = ({ product }: ProductListItemProps) => {
   return (
     <Link href={`/${segments[0]}/menu/${product.id}`} asChild>
     <Pressable style={styles.container}>
-      <Image source={{ uri: product.image || defaultPizzaImage }}
+      <RemoteImage 
+        path={product.image}
+        fallback={defaultPizzaImage}
        style={styles.image} 
        resizeMode="contain"
        />
 
       <Text style={styles.title}>{product.name}</Text>
       <Text style={styles.price}>${product.price}</Text>
-
     </Pressable>
     </Link>
   );
 };
-
-export default ProductListItem;
 
 const styles = StyleSheet.create({
   container: {
@@ -55,3 +55,5 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
 });
+
+export default ProductListItem;
