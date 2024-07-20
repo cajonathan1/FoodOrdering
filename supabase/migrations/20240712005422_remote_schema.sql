a6bf4c9231575e75dd8c86518340e05a5c7df8f6
Jonathan Cruz <cajonathan1@gmail.com>
2024-07-20 09:42:59 -0400

    Fixed TypeError: Network request failed

--- supabase/migrations/20240712005422_remote_schema.sql
+++ supabase/migrations/20240712005422_remote_schema.sql
@@ -100,6 +100,11 @@ end;
 $function$
 ;
 
+-- trigger the function every time a user is created
+create trigger on_auth_user_created
+  after insert on auth.users
+  for each row execute procedure public.handle_new_user();
+
 grant delete on table "public"."order_items" to "anon";
 
 grant insert on table "public"."order_items" to "anon";
