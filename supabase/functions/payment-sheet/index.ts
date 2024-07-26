61b63c5b35943a3f62b7f28a6fc6aa02d3dee443
Jonathan Cruz <cajonathan1@gmail.com>
2024-07-25 21:25:47 -0400

    Developes stripe payment intent deployed via an edge function

--- supabase/functions/payment-sheet/index.ts
+++ supabase/functions/payment-sheet/index.ts
@@ -4,28 +4,34 @@
 
 // Setup type definitions for built-in Supabase Runtime APIs
 import "jsr:@supabase/functions-js/edge-runtime.d.ts"
-import { stripe } from "./_utils/stripe"
+import { stripe } from "./_utils/stripe.ts"
+import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
 
 console.log("Hello from Functions!")
 
-Deno.serve(async (req) => {
-  const { amount } = await req.json()
+serve(async (req) => {
+  try {
+    const { amount } = await req.json()
 
-  const paymentIntent = await stripe.paymentIntent.create({
-    amount: 1099,
-    currency: 'usd',
-  }) 
-
-  const res = {
-    paymentIntent: paymentIntent.client_secret,
-    publishableKey: DelayNode.env.get('EXPO_PUBLIC_STRIPE_PUBLISHABLE_KEY'),
-  };
-
-  return new Response(
-    JSON.stringify(data),
-    { headers: { "Content-Type": "application/json" } },
-  )
-})
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: amount,
+      currency: 'usd',
+    }) 
+    const res = {
+      paymentIntent: paymentIntent.client_secret,
+      publishableKey: Deno.env.get('EXPO_PUBLIC_STRIPE_PUBLISHABLE_KEY'),
+    };
+  
+    return new Response(JSON.stringify(res),
+      { headers: { "Content-Type": "application/json" } },
+    )
+  } catch (error) {
+    return new Response(JSON.stringify(error), {
+      headers: { "Content-Type": "application/json" },
+      status: 400,
+    });
+  }
+});
 
 //  To invoke locally:
   // 1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
@@ -34,4 +40,4 @@ Deno.serve(async (req) => {
   // curl -i --location --request POST 'http://192.168.22.13:54321/functions/v1/payment-sheet' \
   //   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
   //   --header 'Content-Type: application/json' \
-  //   --data '{"name":"Jonathan"}'
+  //   --data '{"amount":1150}'
